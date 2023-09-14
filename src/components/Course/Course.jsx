390af45c07c745120cbd7e59f35b6d9bc9691593
tabib-e-alahi <etabib.alahi@gmail.com>
2023-09-15 00:05:44 +0600

    card select button positon issue fixed

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -6,21 +6,23 @@ const Course = ({course}) => {
     const {course_img,course_name,credit,price,course_details} = course;
     // console.log(course);
     return (
-        <div className='p-4 bg-[#FFF] space-y-4 rounded-xl'>
-            <img className='w-full' src={course_img} alt={`Image of the course ${course_name}`} />
-            <h2 className='text-lg font-semibold'>{course_name}</h2>
-            <p className='h-20 text-[#1C1B1B99] text-sm font-normal'>{course_details}</p>
-            <div className='flex justify-between items-center'>
-                <p className='flex items-center gap-3'>
-                    <img src="/src/assets/dollar-sign 1.png" alt="" />
-                    <span className='text-[#1C1B1B99] text-base'>Price: {price}</span>
-                </p>
-                <p className='flex items-center gap-3'>
-                    <img src="/src/assets/Frame.png" alt="" />
-                    <span className='text-[#1C1B1B99] text-base'>Credit: {credit}hr</span>
-                </p>
+        <div className='p-4 bg-[#FFF]  rounded-xl relative'>
+            <div className='space-y-4 mb-14 '>
+                <img className='w-full' src={course_img} alt={`Image of the course ${course_name}`} />
+                <h2 className='text-lg font-semibold'>{course_name}</h2>
+                <p className='h-20 text-[#1C1B1B99] text-sm font-normal'>{course_details}</p>
+                <div className='flex justify-between items-center '>
+                    <p className='flex items-center gap-3'>
+                        <img src="/src/assets/dollar-sign 1.png" alt="" />
+                        <span className='text-[#1C1B1B99] text-base'>Price: {price}</span>
+                    </p>
+                    <p className='flex items-center gap-3'>
+                        <img src="/src/assets/Frame.png" alt="" />
+                        <span className='text-[#1C1B1B99] text-base'>Credit: {credit}hr</span>
+                    </p>
+                </div>
             </div>
-            <button className='w-full bg-[#2F80ED] py-3 rounded-lg text-white '>Select</button>
+            <button className=' w-11/12 mx-auto  bg-[#2F80ED] py-2 rounded-lg text-white  absolute bottom-4 '>Select</button>
         </div>
     );
 };
--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -14,15 +14,24 @@ const Courses = () => {
         .then(data => setCourses(data))
     },[])
     return (
-        <div className="w-11/12 mx-auto">
-            <div className=" lg:w-3/4 lg:gap-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <div className="w-11/12 mx-auto md:flex gap-6">
+            <div className=" lg:w-3/4 gap-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
                     {
                         courses.map(course => <Course key={course.id} course = {course}></Course>)
                     }
             </div>
 
-            <div className="w-1/3">
+            <div className="w-1/4 p-6 h-1/3 bg-[#FFF]  rounded-xl">
+                    <h1 className="text-lg text-[#2F80ED] font-bold mb-4">Credit Hour Remaining 7 hr</h1>
+                    <hr className="" />
+                    <h2 className="text-lg font-bold mb-4">Course Name</h2>
+                    <div className="mb-6">
 
+                    </div>
+                    <hr />
+                    <p className="font-medium">Total Credit Hour: </p>
+                    <hr />
+                    <p className="font-semibold">Total Price: </p>
             </div>
         </div>
     );
